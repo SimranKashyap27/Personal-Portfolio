625ec681488a4011234ff901f7145b353269c5f4
SimranKashyap27 <kashsim27@gmail.com>
2025-05-20 10:26:14 +0530

    more sections

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var typed= new typed(".text", {
-    strings:["Business Analyst", "Data Analyst"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+// var typed= new typed(".text", {
+//     strings:["Business Analyst", "Data Analyst"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// })
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+      strings: ["Business Analyst", "Data Analyst"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+    });
+  });
